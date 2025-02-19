14ead6339a24debdf9f39f01f0598d0f17da8a86
Martin <martin.bareteau@gmail.com>
2025-02-19 11:34:52 +0100

    Ininit the projet

---
+++ src/main/java/com/FMG/GMF_core/GmfCoreApplication.java
@@ -0,0 +1,13 @@
+package com.FMG.GMF_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmfCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmfCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FMG/GMF_core/GmfCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FMG.GMF_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmfCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
