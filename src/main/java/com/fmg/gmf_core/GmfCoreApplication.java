52a899514be90f351e4675f17fad108f906733e4
Martin <martin.bareteau@gmail.com>
2025-02-21 09:39:12 +0100

    Init the project with src directory now

---
+++ src/main/java/com/fmg/gmf_core/GmfCoreApplication.java
@@ -0,0 +1,13 @@
+package com.fmg.gmf_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmfCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmfCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmg/gmf_core/GmfCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmg.gmf_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmfCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
